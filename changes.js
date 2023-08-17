11f3ce5be9896b7be2d5ac2eb28c1b1b33f828da
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-08-17 11:46:26 +0200

    fix: changes script

--- changes.js
+++ changes.js
@@ -25,9 +25,37 @@ async function fetchVersionOverview() {
     }
 }
 
+async function fetchReleaseNotes(url, versionPath, library, version, requestLog) {
+    try {
+        const { data } = await axios.get(url);
+        await fs.writeFile(path.join(versionPath, `${library}-${version}.json`), JSON.stringify(data, null, 4));
+        if (!requestLog[version]) {
+            requestLog[version] = {};
+        }
+        requestLog[version][library] = true;
+        return true;  // Successfully fetched and saved data
+    } catch (err) {
+        return err;
+    }
+}
+
+
+function compareVersions(v1, v2) {
+    const parts1 = v1.split('.').map(Number);
+    const parts2 = v2.split('.').map(Number);
+    
+    for (let i = 0; i < Math.max(parts1.length, parts2.length); i++) {
+        if (parts1[i] > (parts2[i] || 0)) return 1;
+        if (parts1[i] < (parts2[i] || 0)) return -1;
+    }
+    
+    return 0;
+}
+
+
 async function main() {
     try {
-        this.fetchVersionOverview();
+        await fetchVersionOverview();
     } catch (error) {
         
     }
@@ -43,7 +71,7 @@ async function main() {
     const highestPatchVersions = {};
     for (const patch of versionData.patches) {
         const minorVersion = getMinorVersion(patch.version);
-        if (!highestPatchVersions[minorVersion] || patch.version > highestPatchVersions[minorVersion]) {
+        if (!highestPatchVersions[minorVersion] || compareVersions(patch.version, highestPatchVersions[minorVersion]) > 0) {
             highestPatchVersions[minorVersion] = patch.version;
         }
     }
@@ -59,46 +87,44 @@ async function main() {
 
         for (const library of libraries) {
             const formattedLibrary = library.replace(/\./g, '/');
-            const url = `https://ui5.sap.com/test-resources/${formattedLibrary}/relnotes/changes-${version}.json`;
+            const primaryUrl = `https://ui5.sap.com/${highestPatchVersions[version]}/test-resources/${formattedLibrary}/relnotes/changes-${version}.json`;
+            const fallbackUrl = `https://ui5.sap.com/test-resources/${formattedLibrary}/relnotes/changes-${version}.json`;
 
             const shouldRequest = !requestLog[version] || 
                                  !requestLog[version][library] || 
                                  (requestLog[version].highestPatch && requestLog[version].highestPatch !== highestPatchVersions[version]);
 
             if (shouldRequest) {
-                try {
-                    const { data } = await axios.get(url);
-                    await fs.writeFile(path.join(versionPath, `${library}-${version}.json`), JSON.stringify(data, null, 4));
-                    if (!requestLog[version]) {
-                        requestLog[version] = {};
+                const result = await fetchReleaseNotes(primaryUrl, versionPath, library, version, requestLog);
+    
+                if (result === true) {
+                    // Successful fetch, no further actions needed
+                } else if (result && result.response && result.response.status === 404) {
+                    console.log(`Following URL returned a 404: ${primaryUrl}`);
+                    // Now we try the fallback URL
+                    const fallbackResult = await fetchReleaseNotes(fallbackUrl, versionPath, library, version, requestLog);
+                    if (fallbackResult && fallbackResult.response && fallbackResult.response.status === 404) {
+                        console.log(`Fallback URL also returned a 404: ${fallbackUrl}`);
+                        if (!requestLog[version]) {
+                            requestLog[version] = {};
+                        }
+                        requestLog[version][library] = '404';
                     }
-                    requestLog[version][library] = true;
-
-                } catch (err) {
-                    if (err.response && err.response.status === 404) {
-                        console.log(`Following URL returned a 404: ${url}`);
-                    } else {
-                        console.error(`Error fetching ${url}: ${err.message}`);
-                    }
-                    // Indicate in the requestLog that this URL returned a 404
-                    if (!requestLog[version]) {
-                        requestLog[version] = {};
-                    }
-                    requestLog[version][library] = '404';
-                    
+                } else if (result) {
+                    console.error(`Error fetching ${primaryUrl}: ${result.message}`);
+                    // Handle other errors here if needed
                 }
 
-                // Save to requestLog after every request (regardless of the try-catch outcome)
+                // Save to requestLog after every request (regardless of the outcome)
                 await saveToRequestLog(requestLog, requestLogPath);
 
                 progress++;
                 const percentage = ((progress / totalRequests) * 100).toFixed(2);
                 process.stdout.write(`Progress: ${percentage}%\r`);
 
-                await new Promise(resolve => setTimeout(resolve, 50)); // wait for 50ms before the next request
+                await new Promise(resolve => setTimeout(resolve, 25)); // wait for 50ms before the next request
             }
         }
-
         // After all libraries for this version have been processed:
         if (!requestLog[version]) {
             requestLog[version] = {};
