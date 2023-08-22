2555d11aa3585963e661d8e1f40d5eb5ba66fcab
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-08-22 09:55:39 +0200

    fix: group by version in rss feed

--- dev/createRSS.js
+++ dev/createRSS.js
@@ -13,11 +13,12 @@ function isWithinLastSixMonths(dateStr) {
 
 function jsonToRss(jsonData, rssTitle, rssLink, rssDescription) {
     let rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title>${rssTitle}</title>
 <link>${rssLink}</link>
 <description>${rssDescription}</description>
+<atom:link href="${rssLink}" rel="self" type="application/rss+xml" />
 `;
 
     for (const entry of jsonData) {
@@ -27,19 +28,33 @@ function jsonToRss(jsonData, rssTitle, rssLink, rssDescription) {
         
         const version = entry.version;
         const date = entry.date;
+
+        let description = `Changes in Version ${version}:\n\n`;
+
         for (const lib of entry.libraries) {
             const library = lib.library;
+
+            description += `Library: ${library}\n`;
+
             for (const change of lib.changes) {
                 const changeType = change.type;
                 const changeText = change.text;
-                rssFeed += `<item>
-<title>Version ${version}: ${library} - ${changeType}</title>
+
+                description += `Type: ${changeType}, Description: ${changeText}\n`;
+            }
+
+            description += `\n`;
+        }
+
+        const guid = version;
+
+        rssFeed += `<item>
+<title>Version ${version} Changes</title>
 <pubDate>${new Date(date).toUTCString()}</pubDate>
-<description><![CDATA[${changeText}]]></description>
+<description><![CDATA[${description}]]></description>
+<guid>${guid}</guid>
 </item>
 `;
-            }
-        }
     }
 
     rssFeed += `
@@ -48,6 +63,7 @@ function jsonToRss(jsonData, rssTitle, rssLink, rssDescription) {
 
     return rssFeed;
 }
+
 
 fs.readFile('de.marianzeis.ui5libdiff/webapp/data/consolidated.json', 'utf-8', (err, data) => {
     if (err) {
