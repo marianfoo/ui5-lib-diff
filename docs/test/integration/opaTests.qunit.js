f115c4ba1c995bffb55c13e4a966319479c94d12
marianfoo <13335743+marianfoo@users.noreply.github.com>
2025-01-15 02:27:42 +0000

    chore: build

---
+++ docs/test/integration/opaTests.qunit.js
@@ -0,0 +1,2 @@
+"use strict";sap.ui.define([],function(){"use strict";function e(e){return new Promise(function(t,i){sap.ui.require([e],function(i){if(!(i&&i.__esModule)){i=i===null||!(typeof i==="object"&&e.endsWith("/library"))?{default:i}:i;Object.defineProperty(i,"__esModule",{value:true})}t(i)},function(e){i(e)})})}QUnit.config.autostart=false;void Promise.all([e("integration/HelloJourney")]).then(()=>{QUnit.start()})});
+//# sourceMappingURL=opaTests.qunit.js.map
