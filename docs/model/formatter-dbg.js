20a64e9a9daf869ac95d19d42afb2402a31c3ce2
marianfoo <13335743+marianfoo@users.noreply.github.com>
2025-02-05 02:27:53 +0000

    chore: build

---
+++ docs/model/formatter-dbg.js
@@ -0,0 +1,13 @@
+"use strict";
+
+sap.ui.define([], function () {
+  "use strict";
+
+  var __exports = {
+    formatValue: value => {
+      return value?.toUpperCase();
+    }
+  };
+  return __exports;
+});
+//# sourceMappingURL=formatter-dbg.js.map
